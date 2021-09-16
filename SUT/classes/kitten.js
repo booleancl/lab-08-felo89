6ab6b6e3ef81e2e74e563f467defd7a1594d8ba4
Felipe Pincheira <f.pincheira89@gmail.com>
2021-09-15 21:17:55 -0300

    fix tests

--- SUT/classes/kitten.js
+++ SUT/classes/kitten.js
@@ -6,16 +6,19 @@ class Kitten {
 
   setName(name) {
     this.name = name;
+    return this
   }
 
   setColor(color) {
     this.color = color;
+    return this
   }
 
   save() {
     console.log(
       `saving ${this.name}, the ${this.color} kitten`
     );
+    return this
   }
 }
 
